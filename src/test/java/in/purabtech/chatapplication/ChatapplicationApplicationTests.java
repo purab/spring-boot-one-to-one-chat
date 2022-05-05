f8ddfea12529a915e5241e82dea5d06da55e769e
purab <purabdk@yahoo.co.in>
2022-05-05 22:37:53 +0530

    first commit

---
+++ src/test/java/in/purabtech/chatapplication/ChatapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package in.purabtech.chatapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
